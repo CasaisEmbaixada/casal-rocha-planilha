9a1dfb33d96b764501e3477bd3190546187f532d
Casais Na Rocha <marketingembaixada@gmail.com>
2025-09-05 00:52:37 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Importa o registerSW do plugin PWA
+import { registerSW } from 'virtual:pwa-register'
+
+// Registra o service worker
+registerSW({
+  onNeedRefresh() {
+    console.log("Nova versão disponível!");
+  },
+  onOfflineReady() {
+    console.log("App pronto para uso offline!");
+  },
+})
+
 createRoot(document.getElementById("root")!).render(<App />);
